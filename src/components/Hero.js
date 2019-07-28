4656ba9ecd4358a2ebf4a66f51b05318d274da9d
Alan Bernstein <alan.bernstein@outlook.com>
2019-07-28 19:51:37 +0200

    initial commit

---
+++ src/components/Hero.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Hero extends React.Component {
+
+    render() {
+        return (
+            <React.Fragment>
+
+            </React.Fragment>
+        );
+    }
+}
+
+export default Hero;
