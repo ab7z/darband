8ca653fa12fee2a95828a38469780294746442a9
Alan Bernstein <alan.bernstein@outlook.com>
2019-07-28 20:11:25 +0200

    add hero

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import styles from '../styles/components/Hero.module.css';
 
 class Hero extends React.Component {
 
     render() {
         return (
             <React.Fragment>
+                <div className={styles.hero}>
 
+                </div>
             </React.Fragment>
         );
     }
