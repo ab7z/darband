c266fe15f7362914ac7421bde123f429fdc039af
Alan Bernstein <alan.bernstein@outlook.com>
2019-08-10 00:18:45 +0200

    some styling

--- src/components/Foods.js
+++ src/components/Foods.js
@@ -1,46 +1,50 @@
 import React from 'react';
 import styles from '../styles/components/Foods.module.css';
+import {FormattedMessage} from "react-intl";
 
 class Foods extends React.Component {
     render() {
         return (
             <React.Fragment>
                 <section className={styles.foods}>
-                    <div className={styles.imageContainer}>
-                        <img src={require('../images/1.jpg')} alt=""/>
-                    </div>
-                    <div className={styles.imageContainer}>
-                        <img src={require('../images/2.jpg')} alt=""/>
-                    </div>
-                    <div className={styles.imageContainer}>
-                        <img src={require('../images/3.jpg')} alt=""/>
-                    </div>
-                    <div className={styles.imageContainer}>
-                        <img src={require('../images/4.jpg')} alt=""/>
-                    </div>
-                    <div className={styles.imageContainer}>
-                        <img src={require('../images/1.jpg')} alt=""/>
-                    </div>
-                    <div className={styles.imageContainer}>
-                        <img src={require('../images/2.jpg')} alt=""/>
-                    </div>
-                    <div className={styles.imageContainer}>
-                        <img src={require('../images/3.jpg')} alt=""/>
-                    </div>
-                    <div className={styles.imageContainer}>
-                        <img src={require('../images/4.jpg')} alt=""/>
-                    </div>
-                    <div className={styles.imageContainer}>
-                        <img src={require('../images/1.jpg')} alt=""/>
-                    </div>
-                    <div className={styles.imageContainer}>
-                        <img src={require('../images/2.jpg')} alt=""/>
-                    </div>
-                    <div className={styles.imageContainer}>
-                        <img src={require('../images/3.jpg')} alt=""/>
-                    </div>
-                    <div className={styles.imageContainer}>
-                        <img src={require('../images/4.jpg')} alt=""/>
+                    <h2><FormattedMessage id={"darband.foods.title"}/></h2>
+                    <div className={styles.container}>
+                        <div className={styles.imageContainer}>
+                            <img src={require('../images/1.jpg')} alt=""/>
+                        </div>
+                        <div className={styles.imageContainer}>
+                            <img src={require('../images/2.jpg')} alt=""/>
+                        </div>
+                        <div className={styles.imageContainer}>
+                            <img src={require('../images/3.jpg')} alt=""/>
+                        </div>
+                        <div className={styles.imageContainer}>
+                            <img src={require('../images/4.jpg')} alt=""/>
+                        </div>
+                        <div className={styles.imageContainer}>
+                            <img src={require('../images/1.jpg')} alt=""/>
+                        </div>
+                        <div className={styles.imageContainer}>
+                            <img src={require('../images/2.jpg')} alt=""/>
+                        </div>
+                        <div className={styles.imageContainer}>
+                            <img src={require('../images/3.jpg')} alt=""/>
+                        </div>
+                        <div className={styles.imageContainer}>
+                            <img src={require('../images/4.jpg')} alt=""/>
+                        </div>
+                        <div className={styles.imageContainer}>
+                            <img src={require('../images/1.jpg')} alt=""/>
+                        </div>
+                        <div className={styles.imageContainer}>
+                            <img src={require('../images/2.jpg')} alt=""/>
+                        </div>
+                        <div className={styles.imageContainer}>
+                            <img src={require('../images/3.jpg')} alt=""/>
+                        </div>
+                        <div className={styles.imageContainer}>
+                            <img src={require('../images/4.jpg')} alt=""/>
+                        </div>
                     </div>
                 </section>
             </React.Fragment>
