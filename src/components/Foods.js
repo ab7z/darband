3660f77496ab1642bd4e04105f3b1a128de83d59
Alan Bernstein <alan.bernstein@outlook.com>
2019-10-05 18:19:00 +0200

    testing workflow

--- src/components/Foods.js
+++ src/components/Foods.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import styles from '../styles/components/Foods.module.css';
 import {FormattedMessage} from "react-intl";
+import Modal from 'react-modal';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faTimes} from "@fortawesome/free-solid-svg-icons/faTimes";
 
 class Foods extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            foods: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
-        }
+            foods: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16],
+            isOpen: false
+        };
+        Modal.setAppElement('#root');
+    };
+
+    /**
+     *
+     * @param boolean
+     */
+    setIsOpen = (boolean) => {
+        this.setState({isOpen: boolean});
     };
 
     render() {
@@ -16,13 +29,34 @@ class Foods extends React.Component {
                 <section className={styles.foods}>
                     <h2><FormattedMessage id={"darband.foods.title"}/></h2>
                     <div className={styles.container}>
-                        {this.state.foods.map((food, index)=>
-                            <div className={styles.imageContainer} key={index }>
-                                <img src={require('../images/' + food +'.jpeg')} alt=""/>
+                        {this.state.foods.map((food, index) =>
+                            <div className={styles.imageContainer}
+                                 key={index}
+                                 onClick={() => this.setIsOpen(true)}
+                            >
+                                <img src={require('../images/' + food + '.jpeg')} alt=""/>
                             </div>
                         )}
                     </div>
                 </section>
+                <Modal isOpen={this.state.isOpen}
+                       onRequestClose={() => this.setIsOpen(false)}
+                       contentLabel={"Gerichte"}
+                       style={{
+                           overlay: {
+                               backgroundColor: 'rgba(0, 0, 0, .4)'
+                           }
+                       }}
+                >
+                    <div className={styles.modalContainer}>
+                        <h2>demo</h2>
+                        <FontAwesomeIcon icon={faTimes} size={"2x"}
+                                         onClick={() => {
+                                             this.setIsOpen(false)
+                                         }}
+                        />
+                    </div>
+                </Modal>
             </React.Fragment>
         );
     }
